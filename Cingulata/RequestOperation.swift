0807cb9c4f3dcffa21718cf3229874ed269acdd5
baydet <evsasha182@gmail.com>
2015-12-02 15:24:37 +0300

    added new status codes

--- Cingulata/RequestOperation.swift
+++ Cingulata/RequestOperation.swift
@@ -61,11 +61,18 @@ public enum HTTPStatusCode: Int {
     case OK = 200
     case Created = 201
     case NoContent = 204
+
     case BadRequest = 400
     case Unauthorized = 401
+    case PaymentRequired = 402
     case Forbidden = 403
     case NotFound = 404
+    case MethodNotAllowed = 405
+    case NotAcceptable = 406
+    case RequestTimeout = 408
     case Conflict = 409
+    case Gone = 410
+
     case InternalServerError = 500
 }
 
